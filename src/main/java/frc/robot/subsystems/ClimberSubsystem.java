f5c8975eeaa397fef6b7b6ff72e86eb9b3735d1c
SystemOfParts <97854567+SystemOfParts@users.noreply.github.com>
2024-02-11 14:51:18 -0500

    Possible ClimberSubsystem Fix

--- src/main/java/frc/robot/subsystems/ClimberSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimberSubsystem.java
@@ -13,80 +13,78 @@ import com.revrobotics.CANSparkLowLevel.MotorType;
 import com.revrobotics.SparkRelativeEncoder;
 public class ClimberSubsystem extends SubsystemBase {
 
-  private static final int climber1ID = 16;
-  private static final int climber2ID = 17;
+  private final CANSparkMax climberMotor1 = new CANSparkMax(16, MotorType.kBrushless);
+  private final CANSparkMax climberMotor2 = new CANSparkMax(17, MotorType.kBrushless);
 
-  private CANSparkMax climberMotor1;
-  private CANSparkMax climberMotor2;
+  private final RelativeEncoder climberEncoder1 = climberMotor1.getEncoder();
+  private final RelativeEncoder climberEncoder2 = climberMotor2.getEncoder();
 
-    private RelativeEncoder m_encoder1;
-    private RelativeEncoder m_encoder2;
-   public double arm1zero;
-    public double arm2zero;
+  private static final double GEAR_REDUCTION = 64.0;
+  private static final double ENCODER_RESOLUTION = 42.0; // NEO 550 encoder counts per revolution
+  private static final double AXLE_ROTATION_DISTANCE = 1.0; // Placeholder value for distance traveled per axle rotation (Centimeters)
+  private static final double AXLE_REVOLUTIONS_TO_MAX = 1.0; // Placeholder value for total axle revolutions to maximum value
+  private static final double MAX_POSITION = ENCODER_RESOLUTION * GEAR_REDUCTION * AXLE_ROTATION_DISTANCE * AXLE_REVOLUTIONS_TO_MAX; // Calculate max position
+  private static final double MIN_POSITION = 0.0;
 
   /** Creates a new ClimberSubsystem. */
   public ClimberSubsystem() {
-  
-  climberMotor1 = new CANSparkMax(climber1ID, MotorType.kBrushless);
-  climberMotor2 = new CANSparkMax(climber2ID, MotorType.kBrushless);
 
-    m_encoder1 = climberMotor1.getEncoder(SparkRelativeEncoder.Type.kHallSensor, 42);
+    climberMotor1.restoreFactoryDefaults();
+    climberMotor2.restoreFactoryDefaults();
 
-    m_encoder2 = climberMotor2.getEncoder(SparkRelativeEncoder.Type.kHallSensor, 42);
+    climberMotor1.setIdleMode(CANSparkMax.IdleMode.kBrake);
+    climberMotor2.setIdleMode(CANSparkMax.IdleMode.kBrake);
 
-  climberMotor1.restoreFactoryDefaults();
-  climberMotor2.restoreFactoryDefaults();
-  climberMotor1.setIdleMode(IdleMode.kBrake);
-  climberMotor2.setIdleMode(IdleMode.kBrake);
+    climberMotor1.burnFlash();
+    climberMotor2.burnFlash();
 
-  climberMotor1.burnFlash();
-  climberMotor2.burnFlash();
-   
-  arm1zero = m_encoder1.getPosition();
-  arm2zero = m_encoder2.getPosition();
+    climberEncoder1.setPosition(0);
+    climberEncoder2.setPosition(0);
 
     }
-  public double getarm1position() {
-    return (m_encoder1.getPosition()-arm1zero);
-  }
-   public double getarm2position() {
-    return (m_encoder2.getPosition()-arm2zero);
+
+    public void climber1Up(double speed) {
+      if (climberEncoder1.getPosition() < MAX_POSITION) {
+          climberMotor1.set(speed);
+      } else {
+          climberMotor1.set(0);
+      }
   }
 
-  public void climber1Up() {
-    
-    climberMotor1.set(-.25);
+  public void climber1Down(double speed) {
+      if (climberEncoder1.getPosition() > MIN_POSITION) {
+          climberMotor1.set(-speed);
+      } else {
+          climberMotor1.set(0);
+      }
   }
-  public void climber1Stop() {
+
+  public void climber2Up(double speed) {
+      if (climberEncoder2.getPosition() < MAX_POSITION) {
+          climberMotor2.set(speed);
+      } else {
+          climberMotor2.set(0);
+      }
+  }
+
+  public void climber2Down(double speed) {
+      if (climberEncoder2.getPosition() > MIN_POSITION) {
+          climberMotor2.set(-speed);
+      } else {
+          climberMotor2.set(0);
+      }
+  }
+
+  public void stopClimbers() {
     climberMotor1.set(0);
-  }
-  public void climber1Down() {
-
-    if (getarm1position() < -5){
-    climberMotor1.set(.25);
-    }
-
-  }
-  public void climber2Up() {
-    climberMotor2.set(-.25);
-    }
-  
-  public void climber2Stop() {
     climberMotor2.set(0);
   }
-  public void climber2Down() {
-      if (getarm2position() < -5){
-    climberMotor2.set(.25);
-       }
-  }
 
-  
   @Override
   public void periodic() {
 
-    SmartDashboard.putNumber("Encoderleft", m_encoder1.getPosition());
-        SmartDashboard.putNumber("Encoderright", m_encoder2.getPosition());
-
+    SmartDashboard.putNumber("Encoderleft", climberEncoder1.getPosition());
+    SmartDashboard.putNumber("Encoderright", climberEncoder2.getPosition());
     
     // This method will be called once per scheduler run
   }
