1a0d773222164948e46e0bae23925ecf77b200fc
SystemOfParts <97854567+SystemOfParts@users.noreply.github.com>
2024-02-25 05:08:42 -0500

    Fix for Constants

--- src/main/java/frc/robot/subsystems/LeadScrewSubsystem.java
+++ src/main/java/frc/robot/subsystems/LeadScrewSubsystem.java
@@ -9,6 +9,7 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 import frc.robot.Constants.OrientationConstants.Orientations;
 
 public class LeadScrewSubsystem extends SubsystemBase {
