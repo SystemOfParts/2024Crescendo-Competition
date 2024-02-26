3d8b4b6fc1a9f4e97a8f0738360a87a8ccab909a
Jordan Wilson <jordanwilson@hotmail.com>
2024-02-25 23:38:22 -0500

    deleted empty Extension subsystem

--- src/main/java/frc/robot/subsystems/ExtensionSubsystem.java
+++
@@ -1,9 +0,0 @@
-package frc.robot.subsystems;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import com.revrobotics.CANSparkLowLevel.MotorType;
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.RelativeEncoder;
-
-public class ExtensionSubsystem extends SubsystemBase{
-    
-}
