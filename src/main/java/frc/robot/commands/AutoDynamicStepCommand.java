b590333876dfd223f7d14ec8e096acc410d21d79
Jordan Wilson <jordanwilson@hotmail.com>
2024-03-26 02:33:58 -0400

    AutoDynamicStepCommand

--- src/main/java/frc/robot/commands/AutoDynamicStepCommand.java
+++ src/main/java/frc/robot/commands/AutoDynamicStepCommand.java
@@ -25,36 +25,39 @@ public class AutoDynamicStepCommand extends SequentialCommandGroup {
       String postPath
       )
     {
+    Orientations lastOrientation = null;
     // IF we have a path to travel before we intake
     if (hasPrePath){
       // There was a pre path
       if (hasIntake){
         // There is an intake task
+      lastOrientation = Orientations.AUTO_INTAKE;
       addCommands(
         // together orient to the intake position and start moving 
         new ParallelCommandGroup(
           new AutoMoveToOrientationCommand(m_arm, m_shooter, m_intake, Orientations.AUTO_INTAKE),
           new RunTrajectorySequenceRobotAtStartPoint(prePath)));
       }
-    } else {
-      // There is no intake task
-      addCommands(
-        // just move where we're supposed to go before shooting
-        new RunTrajectorySequenceRobotAtStartPoint(prePath));
-    }
-    // IF we have a note
+    } 
+    //PREPATH COMPLETED OR SKIPPED, WE'VE STOPPED MOVING, HOPEFULLY WE HAVE A NOTE
+    // LET'S CHECK FOR A NOTE
     if (m_intake.isNoteInIntake()){
-      // shoot the note using the current intake
+      // WE HAVE A NOTE
+      // ARE WE SUPPOSED TO MOVE TO A SHOOTING POSITION?
+      if (hasPostPath){
+        // YES - LETS ORIENT AND MOVE TOGETHER
+        addCommands(
+          new ParallelCommandGroup(
+            new AutoMoveToOrientationCommand(m_arm, m_shooter, m_intake, shotOrientation),
+            new RunTrajectorySequenceRobotAtStartPoint(postPath)));
+      } else {
+        // WE DON'T NEED TO MOVE, LET'S JUST ORIENT TO SHOOTING POSITION
+        addCommands(
+          new AutoMoveToOrientationCommand(m_arm, m_shooter, m_intake, shotOrientation));
+      }
+      // WE HAVE FINISHED ORIENTING AND MOVING AS NEEDED LETS USE THIS NOTE
       addCommands(
         new AutoShootAtCurrentTarget(m_arm, m_shooter, m_intake));
     }
-    if (hasPostPath){
-      addCommands(
-        new RunTrajectorySequenceRobotAtStartPoint(postPath));
-    }
-    addCommands(
-      new AutoShootFromSubwoofer(m_arm, m_shooter, m_intake),
-      new AutoMoveToOrientationCommand(m_arm, m_shooter, m_intake, Orientations.TRAVEL)
-    );
   }
 }
